31a33504f94a7cf0ec9651b32405c7cf92a5671b
Nik-Zha <nikhiljhambi@gmail.com>
2025-01-04 14:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    let MinCostPrice = 0;
+
+    function mincost1(arr) { 
+        if(arr.length === 1){
+            return MinCostPrice;
+        }
+        else {
+            let CostArray = [];
+            SortedArray = arr.sort((a,b)=>a-b);
+            CostArray[0] = SortedArray[0] + SortedArray[1];
+            MinCostPrice += CostArray[0];
+            for(let i = 1; i < SortedArray.length - 1; i++){
+                CostArray[i] = SortedArray[i+1];
+            }
+            return mincost1(CostArray);
+        }
+    }
+
+    return mincost1(arr);
 }
 
-module.exports=mincost;
+module.exports = mincost;
